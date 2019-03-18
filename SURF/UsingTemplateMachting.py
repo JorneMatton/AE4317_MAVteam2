8c9325004ba714cb93db15508a4aab67d62ad77b
Jibbam <jipbuwalda@hotmail.com>
2019-03-18 15:49:20 +0100

    added canny

--- SURF/UsingTemplateMachting.py
+++ SURF/UsingTemplateMachting.py
@@ -4,15 +4,18 @@ from matplotlib import pyplot as plt
 import glob
 from scipy.ndimage import rotate
 from collections import deque
+import time
 
 DIST_TRESHOLD = 0.1                     # euclidean distance threshold for the SURF descriptor match filter
-N_SKIP = 1                              # gap in images between image matching reset
-TEMP_SIZE_FACTOR = 2.0                  # ratio factor (template area / feature_size) for template matching 
-OBJECT_SCALE_DETECTION_TH = 1.1         # scale increase detection treshold
-TEMP_MATCH_NUM_OF_SCALE_IT = 10         # number of iterations in the scaling template matching procedure 
+N_SKIP = 2                              # gap in images between image matching reset
+TEMP_SIZE_FACTOR = 2.6                  # ratio factor (template area / feature_size) for template matching 
+OBJECT_SCALE_DETECTION_TH = 1.2         # scale increase detection treshold
+ERROR_DECREASE_FACTOR = 0.7                   # factor by which the template matching error must have improved compared to scale 1
+TEMP_MATCH_NUM_OF_SCALE_IT = 15         # number of iterations in the scaling template matching procedure 
 SURF_HESSIAN_TRESHOLD = 5000            # threshold of hessian for the SURF detection -> depends on image quality
-IS_SURF_UPRIGHT = True                  # Use U-surf to disregard rotation invariance for performance boost
-TEMPLATE_IP_METHOD = cv2.INTER_CUBIC    # template resizing method
+SURF_IS_UPRIGHT = True                  # Use U-surf to disregard rotation invariance for performance boost
+TEMPLATE_IP_METHOD = cv2.INTER_LINEAR   # template resizing method
+EDGE_DETECTOR_IS_ON = True              # Apply edge detector at template matching
 
 prevImgQueue = deque([],N_SKIP)
 prevDescsQueue = deque([],N_SKIP)
@@ -20,20 +23,20 @@ prevKpsQueue = deque([],N_SKIP)
 
 images = [cv2.imread(file) for file in sorted(glob.glob('huistest/*.jpg'))]
 
-for idx,newImg in enumerate(images):
-
-    """ newImg = rotate(newImg,90) """ #optional, depends on footage
+for frameNum,newImg in enumerate(images):
+    
+    """ newImg = rotate(newImg,90) #optional, depends on footage """
     grayNewImg = cv2.cvtColor(newImg, cv2.COLOR_BGR2GRAY)
     (imgHeight,imgWidth) = grayNewImg.shape[:2] 
 
     #obtain Surf features       
-    surf = cv2.xfeatures2d.SURF_create(hessianThreshold = SURF_HESSIAN_TRESHOLD, upright = IS_SURF_UPRIGHT)       
+    surf = cv2.xfeatures2d.SURF_create(hessianThreshold = SURF_HESSIAN_TRESHOLD, upright = SURF_IS_UPRIGHT)       
     (newKps,newDescs) = surf.detectAndCompute(newImg,None)  
     
     #we will store objects in here
     objectKeypoints = []
     
-    if idx > N_SKIP-1: #wait untill queue contains N_SKIP images so we can match with the oldest one
+    if frameNum > N_SKIP-1: #wait untill queue contains N_SKIP images so we can match with the oldest one
 
         #Unpack attributes of the oldest image in the queue to match with
         prevImg = prevImgQueue.popleft()
@@ -81,10 +84,13 @@ for idx,newImg in enumerate(images):
 
                 #create the template around previous keyPoint
                 patchSize = (templateLength, templateLength)                
-                prevTemplate = cv2.getRectSubPix(prevImg,patchSize,prevXYPoint)     
-                                    
+                prevTemplate = cv2.getRectSubPix(prevImg,patchSize,prevXYPoint)
+                if EDGE_DETECTOR_IS_ON:     
+                    prevTemplate = cv2.Canny(prevTemplate, 50, 200)
+
                 lowestError = float('inf')
                 errAtScaleOne = float('inf')
+                bestMatchingScale = 1
 
                 #Loop through template matching procedure by scaling
                 for scale in np.linspace(1.0,1.5,TEMP_MATCH_NUM_OF_SCALE_IT):
@@ -110,13 +116,15 @@ for idx,newImg in enumerate(images):
                     else:                  
 
                         # create the template around the matching keypoint
-                        newTemplate = cv2.getRectSubPix(grayNewImg,resizedPrevTemp.shape,matchPointXY)
+                        newTemplate = cv2.getRectSubPix(grayNewImg,resizedPrevTemp.shape,matchPointXY)                        
+                        if EDGE_DETECTOR_IS_ON:
+                            newTemplate = cv2.Canny(newTemplate, 50, 200)
 
                         """ plt.subplot(1,2,2)
                         plt.imshow(newTemplate,cmap="gray")
                         plt.gca().set_title('img = n')
                         plt.draw()
-                        plt.pause(0.01)  """
+                        plt.pause(0.01) """ 
                         
                         #Compare them using MEAN SQUARED ERROR
                         err = np.sum((resizedPrevTemp.astype("float") - newTemplate.astype("float")) ** 2)
@@ -131,15 +139,14 @@ for idx,newImg in enumerate(images):
                             errAtScaleOne = err                  
                              
                 # Mark the keypoints in the new image frame as objects if the following condition holds
-                if (bestMatchingScale >= OBJECT_SCALE_DETECTION_TH and lowestError <= 0.8*errAtScaleOne):
+                if (bestMatchingScale >= OBJECT_SCALE_DETECTION_TH and lowestError <= ERROR_DECREASE_FACTOR*errAtScaleOne):
                     objectKeypoints.append(finalNewKps[idx])                 
 
         # Draw filtered keypoints (no objects yet)
         plotImg = np.empty((newImg.shape[0], newImg.shape[1], 3), dtype=np.uint8)
-        cv2.drawKeypoints(newImg, finalNewKps,plotImg,(255,0,0),4)
+        cv2.drawKeypoints(newImg, finalNewKps,plotImg,(255,0,0),0)
 
         if len(objectKeypoints) > 0:
-
             # Plot a rectangle covering all obstacle keypoints
             ObjectsXY = [objectKeypoint.pt for objectKeypoint in objectKeypoints]            
             X = [int(objectXY[0]) for objectXY in ObjectsXY]  
@@ -159,6 +166,8 @@ for idx,newImg in enumerate(images):
     prevImgQueue.append(grayNewImg)
     prevDescsQueue.append(newDescs)
     prevKpsQueue.append(newKps)   
+    
+    
 
    
 
